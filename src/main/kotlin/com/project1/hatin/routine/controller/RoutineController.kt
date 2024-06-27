966bbdf04ee6ccd26d631fe0853f6a4483c0e81c
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-06-27 21:10:54 +0900

    유효성 검사 추가

--- src/main/kotlin/com/project1/hatin/routine/controller/RoutineController.kt
+++ src/main/kotlin/com/project1/hatin/routine/controller/RoutineController.kt
@@ -9,11 +9,15 @@ import com.project1.hatin.routine.dto.RoutineResponseDTO.ShowResponseDTO
 import com.project1.hatin.routine.service.RoutineService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+
+@Tag(name = "루틴 Api 컨트롤러", description = "루틴생성, 조회, 수정, 삭제 Api 명세서입니다.")
 @RestController
 @RequestMapping("/api/routine")
 class RoutineController {
@@ -31,7 +35,7 @@ class RoutineController {
 
     @Operation(summary = "사용자 루틴 생성", description = "사용자 루틴 생성")
     @PostMapping
-    private fun postRoutine(@Parameter(description = "사용자가 생성하는 루틴 데이터") @RequestBody createRequestDTO: CreateRequestDTO)
+    private fun postRoutine(@Parameter(description = "사용자가 생성하는 루틴 데이터") @Valid @RequestBody createRequestDTO: CreateRequestDTO)
      : ResponseEntity<BaseResponse<CreateResponseDTO>> {
         val result = routineService.create(createRequestDTO)
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse(data = result))
@@ -40,7 +44,7 @@ class RoutineController {
     @Operation(summary = "사용자 루틴 수정", description = "사용자 루틴 수정")
     @PatchMapping("/{id}")
     private fun patchRoutine(@Parameter(required = true,description = "루틴 ID") @PathVariable(name = "id") id : Long,
-                             @Parameter(description = "사용자가 수정하는 루틴 데이터") @RequestBody patchRequestDTO: PatchRequestDTO )
+                             @Parameter(description = "사용자가 수정하는 루틴 데이터") @Valid @RequestBody patchRequestDTO: PatchRequestDTO )
     : ResponseEntity<BaseResponse<PatchResponseDTO>> {
         val result = routineService.patch(id,patchRequestDTO)
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse(data = result))
