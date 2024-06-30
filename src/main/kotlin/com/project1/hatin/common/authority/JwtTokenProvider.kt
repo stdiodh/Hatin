583173af64f74a7949b29367a8cba905711d6887
stdiodh <playlistdh@gmail.com>
2024-06-30 09:33:52 +0900

    SecurityConfig 파일 생성 + AccessToken 생성

---
+++ src/main/kotlin/com/project1/hatin/common/authority/JwtTokenProvider.kt
@@ -0,0 +1,81 @@
+package com.project1.hatin.common.authority
+
+import com.project1.hatin.common.dto.TokenInfo
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+
+const val EXPIRATION_MILLISECONDS = 1000 * 60 * 30L
+
+@Component
+class JwtTokenProvider {
+    @Value("\$(jwt.secret)")
+    lateinit var secretKey : String
+
+    private val key by lazy { Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey))}
+
+    //토큰 생성
+    fun createAccessToken(authentication : Authentication) : TokenInfo {
+        val authorities : String = authentication
+            .authorities
+            .joinToString(",", transform = GrantedAuthority::getAuthority)
+
+        val now = Date()
+
+        //토큰 유효시간
+        val accessExpriation = Date(now.time + EXPIRATION_MILLISECONDS)
+        val accessToken = Jwts
+            .builder()
+            .subject(authentication.name)
+            .claim("auth", authorities)
+            .issuedAt(now)
+            .expiration(accessExpriation)
+            .signWith(key, Jwts.SIG.HS256)
+            .compact()
+
+        return TokenInfo(grantType = "Bearer", accessToken = accessToken)
+    }
+
+    //토큰 추출
+    fun getAuthentication(token : String) : Authentication {
+        val claims : Claims = getClaims(token)
+        val auth = claims["auth"] ?: throw RuntimeException("유효하지 않은 토큰입니다!")
+
+        val authorities : Collection<GrantedAuthority> = (auth as String)
+            .split(",")
+            .map { SimpleGrantedAuthority(it) }
+
+        val principal : UserDetails = User(claims.subject, "", authorities)
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    //토큰 검증
+    fun validateToken (token: String) : Boolean {
+        try {
+            getClaims(token)
+            return true
+        } catch (e: Exception){
+            println(e.message)
+        }
+        return false
+    }
+
+    //Claims 추출
+    private fun getClaims (token: String) : Claims =
+        Jwts
+            .parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+}
---
+++ src/main/kotlin/com/project1/hatin/common/dto/TokenInfo.kt
@@ -0,0 +1,6 @@
+package com.project1.hatin.common.dto
+
+class TokenInfo (
+    val grantType : String,
+    val accessToken : String,
+)
