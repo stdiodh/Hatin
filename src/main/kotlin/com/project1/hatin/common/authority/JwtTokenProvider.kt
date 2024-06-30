20b631b0973c28e410b8506a2767a4d3ac6ed705
stdiodh <playlistdh@gmail.com>
2024-06-30 09:19:02 +0900

    accessToken 추출, 검증 + SecurityConfig 파일 추가

--- src/main/kotlin/com/project1/hatin/common/authority/JwtTokenProvider.kt
+++ src/main/kotlin/com/project1/hatin/common/authority/JwtTokenProvider.kt
@@ -1,12 +1,17 @@
 package com.project1.hatin.common.authority
 
 import com.project1.hatin.common.dto.TokenInfo
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -40,4 +45,37 @@ class JwtTokenProvider {
 
         return TokenInfo(grantType = "Bearer", accessToken = accessToken)
     }
+
+    //토큰 추출
+    fun getAuthentication(token : String) : Authentication {
+        val claims : Claims = getClaims(token)
+        val auth = claims["auth"] ?: throw RuntimeException("유효하지 않은 토큰입니다!")
+
+        val authorities : Collection<GrantedAuthority> = (auth as String)
+            .split(",")
+            .map { SimpleGrantedAuthority(it) }
+
+        val principal : UserDetails = User(claims.subject, "", authorities)
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    //토큰 검증
+    fun validateToken (token: String) : Boolean {
+        try {
+            getClaims(token)
+            return true
+        } catch (e: Exception){
+            println(e.message)
+        }
+        return false
+    }
+
+    //Claims 추출
+    private fun getClaims (token: String) : Claims =
+        Jwts
+            .parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload
 }
